17a2154c788807e48b40f051fc13ceaeea5275b6
fan160 <167432653+fan160@users.noreply.github.com>
2025-07-04 07:41:42 -0300

    \\\\

--- main.js
+++ main.js
@@ -1,73 +1,72 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j=0; j<botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 5;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
     }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-03-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-09-05T00:00:00");
-const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo4 = new Date("202-02-05T00:00:00");
-
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-
-
-setInterval(atualizaCronometro, 1000);
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    if (tempoFinal > 0){
-    let contador = ' ';
-    contador += '<div class="contador-digito">';
-        contador += '   <p class="contador-digito-numero">'+dias+'</p>';
-        contador += '   <p class="contador-digito-texto">dias</p>';
-        contador += '</div>';
-        contador += '<div class="contador-digito">';
-        contador += '   <p class="contador-digito-numero">'+horas+'</p>';
-        contador += '   <p class="contador-digito-texto">horas</p>';
-        contador += '</div>';
-        contador += '<div class="contador-digito">';
-        contador += '   <p class="contador-digito-numero">'+minutos+'</p>';
-        contador += '   <p class="contador-digito-texto">minutos</p>';
-        contador += '</div>';
-        contador += '<div class="contador-digito">';
-        contador += '   <p class="contador-digito-numero">'+segundos+'</p>';
-        contador += '   <p class="contador-digito-texto">seg</p>';
-        contador += '</div>';
-    return contador;
-
-    } else {
-    return "PRAZO FINALIZADO";
+function aumentaTamanho() {
+    if (tamanhoSenha < 1) {
+        // tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
     }
-
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-function atualizaCronometro(){
-    contadores[0].textContent = calculaTempo(tempoObjetivo1);
-    for (let i=0; i < contadores.length; i++){
-        contadores[i].innerHTML = calculaTempo(tempos[i]);  
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
+
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
     }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+    console.log(alfabeto);
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}   
+function classificaSenha(){
+forcaSenha.classList.remove('fraca','media','forte');
+if (tamanhoSenha > 11){
+forcaSenha.classList.add('forte');
 }
-function comecaCronometro(){
-        atualizaCronometro();
-        setInterval(atualizaCronometro,1000);
+else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
+forcaSenha.classList.add('media');
+}else if (tamanhoSenha <= 5){
+forcaSenha.classList.add('fraca');
 }
-comecaCronometro();
+}
